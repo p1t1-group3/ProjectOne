278f06ff6edc8d281d3c481f1e8ab7cff6dddb60
dsfreehand <daniel.sisson@live.com>
2025-01-17 11:57:17 -0500

    Updated javascript and html to make the Sign Up button function properly. Also made it change to a link to an Order History page if there is a value stored in localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,12 +10,29 @@ const customerEmail  =document.querySelector('.customer-email');
 const customerNumber  =document.querySelector('.customer-number');
 const customerAddress  =document.querySelector('.customer-address');
 const signUPModalBtn= document.querySelector('#userSignUP');
+const acceptBtn = document.getElementById('acceptBtn');
+const homeBtn = document.getElementById('homeB');
+const aboutBtn = document.getElementById('aboutB');
+let UpdatedInfo = JSON.parse(localStorage.getItem('customerInfo'));
 
 
+// I have made UpdatedInfo a global variable so that we can update the signUpModalBtn text content with the user's name.
 
-   signUpBtn.addEventListener('click', function (event) {
+if (UpdatedInfo !== null) {
+  console.log(`You have successfully signed up. Welcome ` + `${UpdatedInfo.Name}`);
+  document.getElementById('userSignUP').textContent = UpdatedInfo.Name + `'s Order History`;
+  document.getElementById('userSignUP').style.fontSize = 'inhert';   
+  signUPModalBtn.addEventListener('click',function(event){
+    window.location.href = 'history.html';
+    // If they have their name saved in local storage, then the Sign Up button changes to a link to their Order History
+  })  
+} else {
+  document.getElementById('userSignUP').textContent = 'Sign Up';
+}
+
+acceptBtn.addEventListener('click',function(event){
   event.preventDefault();
-
+  // Changed this from userSignUP button to acceptBtn which is inside the modal because we don't want to set any values until they click Accept
   const customerInfo = {
     Name: customerName.value,
     Email: customerEmail.value,
@@ -24,18 +41,31 @@ const signUPModalBtn= document.querySelector('#userSignUP');
   };
 
   localStorage.setItem('customerInfo', JSON.stringify(customerInfo));
+
+  UpdatedInfo = customerInfo;
   customerSignUp();
+
 });
 
 function customerSignUp() {
-  const UpdatedInfo = JSON.parse(localStorage.getItem('customerInfo'));
-  if (customerInfo !== null) {
-    console.log("you have successfully signed")
-    document.querySelector('#userSignUP').textContent =
-     UpdatedInfo.customerName+ `  ${UpdatedInfo.phone}`;
-  }
+  
+    console.log(`You have successfully signed up. Welcome ` + `${UpdatedInfo.Name}`);
+    document.getElementById('userSignUP').textContent = UpdatedInfo.Name + `'s Order History`;
+    document.getElementById('userSignUP').style.fontSize = 'inhert';     
+  
 }
 
+// Added link to Home Page
+homeBtn.addEventListener('click',function(event){
+  event.preventDefault();
+  window.location.href = 'index.html'
+});
+
+// Added link to About Page
+aboutBtn.addEventListener('click',function(event){
+  event.preventDefault();
+  window.location.href = 'about.html'
+})
 
           
     
