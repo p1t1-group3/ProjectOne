e038aed2dd3b9902aaa47d9f3677e788fb29ca42
jaguarvusal <jaguarvusal@gmail.com>
2025-01-16 17:09:42 -0800

    working on modal

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,4 @@ const signUp = document.getElementById("signUpModal")
 const order = document.getElementById("orderModal")
 const produce = document.getElementById("product")
 const pizzamenu = document.getElementById("menu")
+
